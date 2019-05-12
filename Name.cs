b008c4a1c20d491e85ddc68cbf400fb015569800
Zac Gross <brickman1444@gmail.com>
2019-05-12 13:32:07 -0500

    Cut out some of the corpus analysis code since it referenced GetQuotes() directly. Get names generating.

--- Name.cs
+++ Name.cs
@@ -1,10 +1,41 @@
+using System.IO;
+using System.Linq;
+
+using Markov;
+
 namespace FantasyCharacterBot
 {
     static class NameGenerator
     {
         public static string GetFullName()
         {
-            return "Elfstar Ravenshed";
+            return GetName() + " " + GetName();
         }
+
+        static string GetName()
+        {
+            MarkovChain<char> chain = GetChain();
+
+            string name = new string(chain.Chain().ToArray());
+
+            return name;
+        }
+
+        static MarkovChain<char> GetChain()
+        {
+            MarkovChain<char> chain = new MarkovChain<char>(2);
+
+            using (StreamReader file = File.OpenText("FantasyCharacterNames.txt"))
+            {
+                string line = file.ReadLine();
+                while (line != null)
+                {
+                    chain.Add(line);
+                    line = file.ReadLine();
+                }
+            }
+            return chain;
+        }
+
     }
 }
