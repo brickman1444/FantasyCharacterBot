f109457b39923ed242b783805a3a19938ecc4663
Zac Gross <brickman1444@gmail.com>
2019-05-12 13:14:45 -0500

    Initial commit of code

---
+++ Name.cs
@@ -0,0 +1,10 @@
+namespace FantasyCharacterBot
+{
+    static class NameGenerator
+    {
+        public static string GetFullName()
+        {
+            return "Elfstar Ravenshed";
+        }
+    }
+}
